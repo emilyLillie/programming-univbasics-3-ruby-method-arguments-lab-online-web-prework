af8317571f0f37e1d2f3cd441fea9ae42721bac9
Emily Lillie <emilylillie3@gmail.com>
2019-06-16 20:33:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,5 +10,5 @@ end
 #language = "Ruby"
 def introduction_with_language_optional(name, language={})
   #language = "Ruby"
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
